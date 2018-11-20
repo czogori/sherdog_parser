a3be41cdc6988923f41bfc3fb740e51bcd587f33
Arek Jaskólski <arek.jaskolski@gmail.com>
2018-11-20 22:07:16 +0100

    Improved tests

--- test/sherdog_parser_test.exs
+++ test/sherdog_parser_test.exs
@@ -9,7 +9,13 @@ defmodule SherdogParserTest do
 
   test "parse a fighter", state do
     fighter = SherdogParser.parse_fighter(state.fighter_page)
+
     assert "Mamed Khalidov" == fighter.name
+    assert ~D[1980-07-17] == fighter.birthday
+    assert {"Poland", "Olsztyn"} == fighter.birthplace
+    assert 183 == fighter.height
+    assert 84 == fighter.weight
+    assert 41 == fighter.fights |> Enum.count()
   end
 
   test "find ids of fighters", state do
