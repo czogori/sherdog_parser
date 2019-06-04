338a4c811d4318bb6f6db6e9bd4e444a68f5f348
Arek Jaskólski <arek.jaskolski@gmail.com>
2019-06-04 23:31:33 +0200

    Delegate functions

--- lib/sherdog_parser.ex
+++ lib/sherdog_parser.ex
@@ -5,7 +5,7 @@ defmodule SherdogParser do
 
   alias SherdogParser.{EventParser, FighterParser, OrganizationParser}
 
-  def event(html), do: EventParser.parse(html)
-  def fighter(html), do: FighterParser.parse(html)
-  def organization(html), do: OrganizationParser.parse(html)
+  defdelegate event(html), to: EventParser, as: :parse
+  defdelegate fighter(html), to: FighterParser, as: :parse
+  defdelegate organization(html), to: OrganizationParser, as: :parse
 end
