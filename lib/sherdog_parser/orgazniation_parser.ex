ab149f7a605032c25c6491e795a14bf95e010403
Arek Jaskólski <arek.jaskolski@gmail.com>
2019-06-08 23:24:14 +0200

    Removed unused code

--- lib/sherdog_parser/orgazniation_parser.ex
+++ lib/sherdog_parser/orgazniation_parser.ex
@@ -32,22 +32,4 @@ defmodule SherdogParser.OrganizationParser do
         :err
     end
   end
-
-  def parse_date(year, month, day)
-      when is_bitstring(year) and is_bitstring(month) and is_bitstring(year) do
-    Date.from_erl!({String.to_integer(year), month(month), String.to_integer(day)})
-  end
-
-  defp month("Jan"), do: 1
-  defp month("Feb"), do: 2
-  defp month("Mar"), do: 3
-  defp month("Apr"), do: 4
-  defp month("May"), do: 5
-  defp month("Jun"), do: 6
-  defp month("Jul"), do: 7
-  defp month("Aug"), do: 8
-  defp month("Sep"), do: 9
-  defp month("Nov"), do: 10
-  defp month("Dec"), do: 11
-  defp month("Oct"), do: 12
 end
